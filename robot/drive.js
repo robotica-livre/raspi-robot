33b9321f09db2395073c7c2b7149bea3e1d4af7d
Ricardo Martinelli de Oliveira <ricardo.martinelli.oliveira@gmail.com>
2014-12-27 00:07:08 +0000

    Adding a pi-gpio implementation of drive.js

--- robot/drive.js
+++ robot/drive.js
@@ -3,30 +3,48 @@
  * 
  * The objective of it is encapsulate robot
  * drive behavior.
+ * 
+ * TODO: Need to find a way to implement PWM to control the motors.
+ * One possible way is implement a native wiringPi class and convert to
+ * a Node.js library.
+ * 
+ * References:
+ * * http://www.embarcados.com.br/gerando-pwm-com-raspberry-pi/
+ * * http://syskall.com/how-to-write-your-own-native-nodejs-extension/index.html/
+ * 
  */
-var RaspiRobot = require("raspirobot").RaspiRobot, // Import the library
-    drive = new RaspiRobot();
+var gpio = require("pi-gpio");
 
 function Drive(left, right) {
-	drive.setup();
+	this.setup();
+}
+
+Drive.prototype.setup = function(left, right) {
+	this.left = left;
+	gpio.open(left);
+	gpio.setDirection(left, "output");
+	
+	this.right = right;
+	gpio.open(right);
+	gpio.setDirection(right, "output");
 }
 
 Drive.prototype.forward = function() {
-	drive.setMotor("left", 1, 1);
-	drive.setMotor("right", 1, 1);
+	this.write(left, 1);
+	this.write(right, 1);
 };
 
 Drive.prototype.backward = function() {
-	drive.setMotor("left", 0, 0);
-	drive.setMotor("right", 0, 0);
+	this.write(left, 0, 0);
+	this.write(right, 0, 0;)
 };
 
 Drive.prototype.turnLeft = function() {
-	drive.setMotor("left", 0);
-	drive.setMotor("right", 1, 1);
+	this.write(left, 0);
+	this.write(right, 1);
 };
 
 Drive.prototype.turnRight = function() {
-	drive.setMotor("left", 1, 1);
-	drive.setMotor("right", 0);
+	this.write(left, 1);
+	this.write(right, 0);
 };
